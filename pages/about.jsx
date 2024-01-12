c704fdba1c5052a904dbd21088b342ad98ab154f
Alex Raymond <alexjacobraymond@gmail.com>
2024-01-11 22:00:18 -0800

    added image to about me

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,13 @@
+import styles from "../styles/About.module.css"
+
 const AboutPage = () => {
   return (
     <>
       <h3>A Little Bit About Me</h3>
 <body>
-    <div id="bio">
-        <p>Hello, my name is <strong>Alex Raymond</strong>. I am a passionate and innovative web developer with a rich academic background, holding a bachelor's degree in History and a double minor in Computer Science and Web Programming & Applications. My educational journey has equipped me with a unique perspective on tech and its historical context, enabling me to approach web development with a fresh and multifaceted mindset.</p>
+    <div id="bio" className={styles.AboutPage}>
+      <img src="https://i.imgur.com/zX3b9qj.png" />
+        <div>Hello, my name is <strong>Alex Raymond</strong>. I am a passionate and innovative web developer with a rich academic background, holding a bachelor's degree in History and a double minor in Computer Science and Web Programming & Applications. My educational journey has equipped me with a unique perspective on tech and its historical context, enabling me to approach web development with a fresh and multifaceted mindset.</div>
 
         <p>My passion extends beyond coding and algorithms; I am deeply involved in the world of improv comedy. With several years of experience under my belt, I have not only honed my skills as a performer but also as a coach and instructor. I am proud to be part of dynamic teams that perform across various esteemed stages in Los Angeles, Orange County, and nationally at renowned comedy festivals. This experience in improv has sharpened my ability to think on my feet and bring a creative twist to all my web development projects.</p>
 
